45c4e0083e2023c898e7e3474c0474c7c920206f
Noe Giovinazzi <noe.giovinazzi@gmail.com>
2022-05-25 19:27:22 +0200

    changed app

--- service.py
+++ service.py
@@ -4,17 +4,28 @@ import time
 time.sleep(10)
 print("Started:")
 #Add your own SQL Server IP address, PORT, UID, PWD and Database
-conn_string = 'DRIVER={FreeTDS};SERVER=sqlserver;PORT=1433;DATABASE=Master;UID=SA;PWD=mssql1Ipw'
+conn_string = 'DRIVER={FreeTDS};SERVER=sqlserver;PORT=1433;DATABASE=tempdb;UID=SA;PWD=mssql1Ipw'
 print(conn_string)
 conn = pyodbc.connect(
     conn_string, autocommit=True)
 cur = conn.cursor()
 #This is just an example
 cur.execute(
-    f"SELECT [name] FROM sys.databases")
-#conn.commit()
-dbs = cur.fetchall()
+    f"CREATE TABLE products (product_name nvarchar(50),price int)")
 
-print(dbs)
+print('done table')
+insert = "INSERT INTO products (product_name, price) VALUES "
+i = 0
+while i < 1000:
+    i += 1
+    chu = f"('Item {i}',{i})"
+    insert = insert + chu
+    if i < 1000:
+        insert = insert + ", "
+conn.execute(insert)      
+#conn.commit()
+#dbs = cur.fetchall()
+
+print('done')
 cur.close()
 conn.close()
